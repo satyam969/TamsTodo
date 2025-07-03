882c937ff873c3e156d264d267386ccb20cc767f
satyam969 <satyamtiwari87090@gmail.com>
2025-07-03 14:07:53 +0530

    slight

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
+     base: './',
     plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
